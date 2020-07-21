36c4c071683aa2bb1e967d31ddd2ecee517d6120
Liadrinz <435438602@qq.com>
2020-07-21 22:23:51 +0800

    move examples to a dir

---
+++ examples/bupt.py
@@ -0,0 +1,73 @@
+import re
+import json
+
+from base64 import b64encode
+
+from jsonflow.decorators import src, flow, thread, register
+from jsonflow.access import get_data
+from jsonflow.core import jflow
+
+from bs4 import BeautifulSoup
+from flask import Flask, request, render_template, abort
+
+@register
+def parse_info(username, password):
+    return (b64encode(username.encode()) + b'%%%' + b64encode(password.encode())).decode()
+
+def trim(s):
+    return re.sub(r'\s', '', s)
+
+def avg(courses):
+    score_total = 0
+    point_total = 0
+    for course in courses:
+        if course.get('课程属性', '') == '任选':
+            continue
+        try:
+            score = float(course.get('成绩', 0))
+            point = float(course.get('学分', 0))
+        except ValueError:
+            continue
+        score_total += score * point
+        point_total += point
+    return score_total / point_total
+
+app = Flask(__name__)
+
+@src(
+    url='http://jwgl.bupt.edu.cn/jsxsd/xk/LoginToXk',
+    method='post',
+    data={
+        'userAccount': '<username>',
+        'userPassword': '',
+        'encoded': '<self.parse_info(username, password)>'
+    }
+)
+@src('http://jwgl.bupt.edu.cn/jsxsd/kscj/cjcx_list')
+@flow(
+    lambda raw : BeautifulSoup(raw, 'lxml'),
+    lambda soup : soup.find('table'),
+    [lambda table : [trim(th.text) for th in table.find_all('th')], lambda table : table.find_all('tr')],
+    [lambda result: result[0], lambda result : [[trim(td.text) for td in row.find_all('td')] for row in result[1]]]
+)
+def get_avg(username, password):
+    courses = []
+    headers, rows = get_data()
+    for row in rows:
+        courses.append(dict(zip(headers, row)))
+    return avg(courses)
+
+@app.route('/', methods=['GET', 'POST'])
+def average():
+    if request.method == 'GET':
+        return render_template('index.html', info='')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    try:
+        score = get_avg(username=username, password=password)
+        return render_template('index.html', info=f'加权平均分：{score}')
+    except:
+        return render_template('index.html', info='学号或密码错误，或者是你没挂VPN')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8888, debug=True)
---
+++ examples/example.py
@@ -0,0 +1,56 @@
+import re
+
+from jsonflow.decorators import src, flow, thread
+from jsonflow.access import get_data
+from jsonflow.core import jflow
+from jsonflow import config
+
+from bs4 import BeautifulSoup
+
+config.max_workers = 4  # 线程数
+
+def preprocess_item(html):
+    data = {}
+    soup = BeautifulSoup(html, 'lxml')
+    basicInfoBox = soup.find('div', {'class': 'basic-info'})
+    for dl in basicInfoBox.findAll('dl'):
+        for k, v in zip(dl.findAll('dt'), dl.findAll('dd')):
+            data[k.text] = v.text
+    return data
+
+@thread(callback=lambda data : print(data))
+@src('https://baike.baidu.com/item/<name>', inherit_cookies=True)
+@flow(
+    preprocess_item,
+    lambda data: {re.sub('\s', '', k) : re.sub('\s', '', data[k]) for k in data}
+)
+def get_abstract(name):
+    return get_data()
+
+@src(
+    'http://localhost:8000/login',
+    method='post',
+    data = {
+        'username': 'Liadrinz',
+        'password': '123456'
+    }
+)
+def login():
+    return get_data()
+
+@src('http://localhost:8000/test_data', inherit_cookies=True)
+def get_greet():
+    return get_data()
+
+def test_abstract():
+    for keyword in ['c++', 'python', 'java', 'c#', 'javascript']:
+        get_abstract(name=keyword)
+
+def test_greeting():
+    data = get_greet()
+    print(data)
+
+if __name__ == '__main__':
+    test_abstract()
+    login()
+    test_greeting()
